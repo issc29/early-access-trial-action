eac377d5eca378340a5fbc0207d44093b9bed6f1
Isaac Cohen <issc29@gmail.com>
2021-03-25 13:48:43 -0400

    commit

--- index.js
+++ index.js
@@ -2,53 +2,61 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const dedent = require('dedent');
 
+const myToken = core.getInput('github-token');
+const octokit = github.getOctokit(myToken)
+const approve = core.getInput('approve');
+const scRequestedLabledID = core.getInput('requestedLabelID');
+const issueID = payload.client_payload.command.resource.id
+  
+const addCommentMutation = `mutation addComment($issueId: ID!, $commentBody: String!){ 
+  addComment(input:{subjectId: $issueId , body: $commentBody}) {
+    commentEdge {
+      node {
+        id
+      }
+    }
+    }
+  }`;
+  
+  const addLabelMutation = `mutation addLabel($issueId: ID!, $labelId: [ID!]!){ 
+  addLabelsToLabelable(input:{labelIds:$labelId, labelableId:$issueId}){
+    labelable {
+      ... on Issue {
+        id
+      }
+    }
+  }
+}`;
+
+const getIssueInfo = `query($issueId: ID!) { 
+  node(id:$issueId) {
+    ... on Issue {
+      title,
+      number
+    }
+  }
+}`;
+
+
 run();
 
 async function run() {
-  try {
-    const myToken = core.getInput('github-token');
-    const octokit = github.getOctokit(myToken)
 
-    const scRequestedLabledID = core.getInput('requestedLabelID');
-    const payload = github.context.payload
-
-    const issueID = payload.client_payload.command.resource.id
-
-    const comment = dedent`
+  const comment = dedent`
     Early Access Name: ${payload.client_payload.data['Early Access Name']}
-      * Is this an existing customer or prospect? ${payload.client_payload.data['Briefed of Functionality?']}
-      * [Prospect] Have they been briefed on the functionality of Security Center today? ${payload.client_payload.data['Existing GHAS Customer?']}
-      * [Prospect] Is Security Center critical to the success of the POC? ${payload.client_payload.data['Critical to POC?']}
-      * [Prospect] Comments: ${payload.client_payload.data['Comments']}
-        
-    Next steps: Needs approval by @niroshan or @issc29`
-      
-    const addCommentMutation = `mutation addComment($issueId: ID!, $commentBody: String!){ 
-      addComment(input:{subjectId: $issueId , body: $commentBody}) {
-        commentEdge {
-          node {
-            id
-          }
-        }
-        }
-      }`;
-      
-      const addLabelMutation = `mutation addLabel($issueId: ID!, $labelId: [ID!]!){ 
-      addLabelsToLabelable(input:{labelIds:$labelId, labelableId:$issueId}){
-        labelable {
-          ... on Issue {
-            id
-          }
-        }
-      }
-    }`;
+    * Is this an existing customer or prospect? ${payload.client_payload.data['Briefed of Functionality?']}
+    * [Prospect] Have they been briefed on the functionality of Security Center today? ${payload.client_payload.data['Existing GHAS Customer?']}
+    * [Prospect] Is Security Center critical to the success of the POC? ${payload.client_payload.data['Critical to POC?']}
+    * [Prospect] Comments: ${payload.client_payload.data['Comments']}
     
+    Next steps: Needs approval by @niroshan or @issc29`
+
+  try {
+
+    // Add Comment to current Issue
     const commentVariables = {
       issueId: issueID,
       commentBody: comment,
-      headers: {
-        Accept: `application/vnd.github.starfox-preview+json`
-      }
     }
     const commentResult = await octokit.graphql(addCommentMutation, commentVariables)
     if (!commentResult) {
@@ -57,6 +65,8 @@ async function run() {
     else {
       console.log(`Added Comment to Issue: issueID, nodeId: ${commentResult.addComment.commentEdge.node.id}`)
     } 
+
+    // Add Label to current Issue
     const labelVariables = {
       issueId: issueID,
       labelId: scRequestedLabledID
@@ -67,6 +77,37 @@ async function run() {
     } 
     else {
       console.log(`Added Label to Issue: issueID, nodeId: ${labelResult.addLabelsToLabelable.labelable.id}`)
+    } 
+
+    //Get Issue Title
+    const getIssueInfoVariables = {
+      issueId: issueID
+    }
+    const getIssueInfoResult = await octokit.graphql(getIssueInfo, getIssueInfoVariables)
+    if (!getIssueInfoResult) {
+      core.setFailed('GraphQL request failed')
+    } 
+    else {
+      console.log(`Title: ${getIssueInfo.node.title}`)
+    } 
+
+    const requestedIssueID = core.getInput('requestedIssueID')
+
+    // Add Comment to Requested Issue
+
+    const requestIssueComment = dedent`
+    New Early Access Request: ${getIssueInfo.node.title} #${getIssueInfo.node.number}`
+
+    const requestedIssueCommentVariables = {
+      issueId: requestedIssueID,
+      commentBody: requestIssueComment,
+    }
+    const requestedIssueResult = await octokit.graphql(addCommentMutation, requestedIssueCommentVariables)
+    if (!requestedIssueResult) {
+      core.setFailed('GraphQL request failed')
+    } 
+    else {
+      console.log(`Added Label to Issue: issueID, nodeId: ${requestedIssueResult.addLabelsToLabelable.labelable.id}`)
     } 
 
   } catch (error) {
