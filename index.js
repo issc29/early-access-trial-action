76d61dc4e91020e99b1d935a58f7850c06a74188
Isaac Cohen <issc29@gmail.com>
2021-03-25 17:16:54 -0400

    commit

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const octokit = github.getOctokit(myToken)
 const scRequestedLabledID = core.getInput('requestedLabelID');
 const payload = github.context.payload
 const issueID = payload.client_payload.command.resource.id
-  
+
+const functionsLib = require('./functions');
+var functions = new functionsLib(octokit, core)
 
 run();
 
@@ -17,10 +19,10 @@ async function run() {
 
     // Add Comment to current Issue
     const currentIssueComment = getCurrentIssueComment(payload.client_payload.data)
-    await commentOnIssue(issueID, currentIssueComment)
+    await functions.commentOnIssue(issueID, currentIssueComment)
 
     // Add Label to current Issue
-   await addLabelToIssue(issueID, scRequestedLabledID)
+   await functions.addLabelToIssue(issueID, scRequestedLabledID)
 
     //Get Issue Title
     const issueInfo = await getIssueInfo(issueID)
@@ -28,7 +30,7 @@ async function run() {
 
     // Add Comment to Requested Issue
     const requestIssueComment = getRequestIssueComment(issueInfo)
-    await commentOnIssue(requestedIssueID, requestIssueComment)
+    await functions.commentOnIssue(requestedIssueID, requestIssueComment)
 
   } catch (error) {
     core.setFailed(error.message);
@@ -50,76 +52,4 @@ function getCurrentIssueComment(payloadData){
 function getRequestIssueComment(issueInfo){
   return dedent`
     New Early Access Request: ${issueInfo.title} #${issueInfo.number}`
-}
-
-async function commentOnIssue(issueID, comment) {
-  const addCommentMutation = `mutation addComment($issueId: ID!, $commentBody: String!){ 
-    addComment(input:{subjectId: $issueId , body: $commentBody}) {
-      commentEdge {
-        node {
-          id
-        }
-      }
-      }
-    }`;
-
-  const variables = {
-    issueId: issueID,
-    commentBody: comment,
-  }
-  const result = await octokit.graphql(addCommentMutation, variables)
-  if (!result) {
-    core.setFailed('GraphQL request failed')
-  } 
-
-  return result
-}
-
-
-async function getIssueInfo(issueID) {
-  const getIssueInfoQuery = `query($issueId: ID!) { 
-    node(id:$issueId) {
-      ... on Issue {
-        title,
-        number
-      }
-    }
-  }`;
-
-  const variables = {
-    issueId: issueID
-  }
-  const result = await octokit.graphql(getIssueInfoQuery, variables)
-  if (!result) {
-    core.setFailed('GraphQL request failed')
-  } 
-  else {
-    console.log(`Title: ${result.node.title}`)
-  } 
-  return result.node
-}
-
-async function addLabelToIssue(issueID, labelID) {
-  const addLabelMutation = `mutation addLabel($issueId: ID!, $labelId: [ID!]!){ 
-    addLabelsToLabelable(input:{labelIds:$labelId, labelableId:$issueId}){
-      labelable {
-        ... on Issue {
-          id
-        }
-      }
-    }
-  }`;
-
-  const variables = {
-    issueId: issueID,
-    labelId: labelID
-  }
-  const result = await octokit.graphql(addLabelMutation, variables)
-  if (!result) {
-    core.setFailed('GraphQL request failed')
-  } 
-  else {
-    console.log(`Added Label: nodeId: ${result.addLabelsToLabelable.labelable.id}`)
-  } 
-  return result.addLabelsToLabelable.labelable
 }
